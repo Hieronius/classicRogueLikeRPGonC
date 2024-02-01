0da9942ab62c1b1ec53e4f1d630f6e2ea2a5a80e
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-01 06:34:44 +0300

    Add player initialization and edit while cycle

--- classicRogueLikeRPGonC/src/main.c
+++ classicRogueLikeRPGonC/src/main.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <ncurses.h>
-#include <stdlib.h>
-#include <string.h>
 #include "rogue.h"
 
+Entity *player; // external variable from rogue.h
+
 int main() {
+    int character = 0; // variable to store user's input
+    Position startPosition = {10, 20}; // hardcode starting point of the player
     
     initscr(); // starts up the ncurses system and allows us to call all other functions in our terminal
     
@@ -12,11 +12,19 @@ int main() {
 
     curs_set(0); // will make our cursor invisible
     
-    while (getch() != 'q') { // getch() - will get single character from the keyboard and return it without pressing "Enter"
+    player = createPlayer(startPosition);
+    mvaddch(player->position.y, player->position.x, player->character); // function to print (draw) element in (y, x) coordinates
+    
+    while (character == getch()) { // getch() - will get single character from the keyboard and return it without pressing "Enter"
         
         // So, until user will press 'q' game will draw '@' each new button that has been pressed
+        if (character == 'q') {
+            break;
+        }
         
-        mvaddch(10, 20, '@'); // function to print (draw) element in (y, x) coordinates
+        handleInput(character);
+        clear();
+        mvaddch(player->position.y, player->position.x, player->character);
     }
     
     endwin(); // ncurses function that closes ncurses in our terminal
