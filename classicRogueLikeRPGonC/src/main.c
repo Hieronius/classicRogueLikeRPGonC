5cce51b723927dbf3e95d89e89bb518566b356ef
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-01 19:00:43 +0300

    Add map initialization to the main.c

--- classicRogueLikeRPGonC/src/main.c
+++ classicRogueLikeRPGonC/src/main.c
@@ -4,12 +4,14 @@ const int MAP_HEIGHT = 25;
 const int MAP_WIDTH = 100;
 
 Entity *player; // external variable from rogue.h
+Tile **map;
 
 int main(void) {
     cursesSetup();
     
     Position startPosition = {10,20};
     player = createPlayer(startPosition);
+    map = createMapTiles();
     
     gameLoop();
     closeGame();
