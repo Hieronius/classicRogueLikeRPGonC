02cfcada201bc6c603cac7ee1b7e381440c1561e
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-02 03:52:31 +0300

    Replace mvaddch() with drawEverything method

--- classicRogueLikeRPGonC/src/engine.c
+++ classicRogueLikeRPGonC/src/engine.c
@@ -11,7 +11,7 @@ void cursesSetup(void) {
 void gameLoop(void) {
     int character; // variable to store user's input
     
-    mvaddch(player->position.y, player->position.x, player->character);
+    drawEverything(); // clean the screen, draw map and the player.
     
     while ((character = getch())) { // getch() - will get single character from the keyboard and return it without pressing "Enter"
         
@@ -23,8 +23,7 @@ void gameLoop(void) {
         }
         
         handleInput(character);
-        clear();
-        mvaddch(player->position.y, player->position.x, player->character);
+        drawEverything();
     }
 }
 
