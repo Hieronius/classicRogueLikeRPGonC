def7322e768ece977b30dd17a217b8576c72e82e
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-03 16:03:05 +0300

    Define color of the tile/entity during the draw period

--- classicRogueLikeRPGonC/src/draw.c
+++ classicRogueLikeRPGonC/src/draw.c
@@ -3,13 +3,13 @@
 void drawMap(void) {
     for (int y = 0; y < MAP_HEIGHT; y++) {
         for (int x = 0; x < MAP_WIDTH; x++) {
-            mvaddch(y, x, (*(*(map + y) + x)).character);
+            mvaddch(y, x, (*(*(map + y) + x)).character | (*(*(map + y) + x)).color); // returns the same tile but with different color
         }
     }
 }
 
 void drawEntity(Entity *entity) {
-    mvaddch(entity->position.y, entity->position.x, entity->character);
+    mvaddch(entity->position.y, entity->position.x, entity->character | entity->color); // returns the same character but with different color
 }
 
 void drawEverything(void) {
