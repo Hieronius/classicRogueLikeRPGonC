76f3b41638cdc929f5a5ffda46631031a39daf84
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-03 09:52:35 +0300

    Add methods to create rooms

--- classicRogueLikeRPGonC/src/room.c
+++ classicRogueLikeRPGonC/src/room.c
@@ -14,6 +14,42 @@ Room createRoom(int y, int x, int height, int width) {
     return newRoom;
 }
 
+void connectRoomCenters(Position centerOne, Position centerTwo) {
+
+    Position temp;
+    temp.x = centerOne.x; // coordinates of the first room
+    temp.y = centerOne.y; // to connect from
+
+    // move temp position until it reach the coordinates of the center of second room and stop the loop.
+    while (true) {
+
+        // if centerOneX is bigger than centerTwoX let's decrement centerOne.x
+        if (abs((temp.x - 1) - centerTwo.x) < abs(temp.x - centerTwo.x)) {
+            temp.x--;
+            
+        // if centerOneX is smaller than centerTwoX let's increment centerOne.x
+        } else if (abs((temp.x + 1) - centerTwo.x) < abs(temp.x - centerTwo.x)) {
+            temp.x++;
+
+        // if centerOneY if smaller than centerTwoY let's increment centerOne.y
+        } else if (abs((temp.y + 1) - centerTwo.y) < abs(temp.y - centerTwo.y)) {
+            temp.y++;
+         
+        // if centerOneY is bigger than centerTwoY let's decrement centerOne.y
+        } else if (abs((temp.y - 1) - centerTwo.y) < abs(temp.y - centerTwo.y)) {
+            temp.y--;
+        
+        // If centerOne = centerTwo let's brake the loop.
+        } else {
+            break;
+        }
+        
+        // each iteration draw walkable tile of new coordinate we have reached to
+        (*(*(map + temp.y) + temp.x)).character = '.';
+        (*(*(map + temp.y) + temp.x)).walkable = true;
+    }
+}
+
 void addRoomToMap(Room room) {
     for (int y = room.position.y; y < room.position.y + room.height; y++) {
         for (int x = room.position.x; x < room.position.x + room.width; x++) {
@@ -22,3 +58,5 @@ void addRoomToMap(Room room) {
         }
     }
 }
+
+
