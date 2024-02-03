3565773d0aaf280f0381f9eab4d20052cb9cdc1a
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-03 16:02:40 +0300

    Add color pairs for Ncurses and color property for Tiles and Entity

--- classicRogueLikeRPGonC/src/include/rogue.h
+++ classicRogueLikeRPGonC/src/include/rogue.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+// color pairs
+#define VISIBLE_COLOR 1 
+#define SEEN_COLOR 2
+
 // MARK: - Structs
 
 typedef struct {
@@ -14,6 +18,7 @@ typedef struct {
 
 typedef struct {
     char character; // character to represent a specific tiles on the map
+    int color; // store a result of ncurses function COLOR_PAIR()
     bool walkable; // player can walk on floors but not  through the walls
 } Tile; // represent toles of the map (should be represented by two dimensional-array)
 
@@ -27,6 +32,7 @@ typedef struct {
 typedef struct {
     Position position; // current hero's position
     char character; // representation of the hero on screen
+    int color; // store a result of ncurses function COLOR_PAIR()
 } Entity;
 
 // MARK: - Methods
