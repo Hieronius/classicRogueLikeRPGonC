fdceccfc65173fe893213a2b474c272f9f03cd4c
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-04 06:21:26 +0300

    Remove unused code

--- classicRogueLikeRPGonC/src/draw.c
+++ classicRogueLikeRPGonC/src/draw.c
@@ -3,13 +3,13 @@
 void drawMap(void) {
     for (int y = 0; y < MAP_HEIGHT; y++) {
         for (int x = 0; x < MAP_WIDTH; x++) {
-            mvaddch(y, x, (*(*(map + y) + x)).character | (*(*(map + y) + x)).color); // returns the same tile but with different color
             
             if ((*(*(map + y) + x)).visible) {
                 mvaddch(y, x, (*(*(map + y) + x)).character | (*(*(map + y) + x)).color);
                 
             } else if ((*(*(map + y) + x)).seen) {
                     mvaddch(y, x, (*(*(map + y) + x)).character | COLOR_PAIR(SEEN_COLOR));
+                
             } else {
                 mvaddch(y, x, ' ');
             }
