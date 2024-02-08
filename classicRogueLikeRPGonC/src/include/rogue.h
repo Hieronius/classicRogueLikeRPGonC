fd1bc04ee9cd5c541ed474552825b0cc97803493
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-08 04:47:34 +0300

    Add struct titles

--- classicRogueLikeRPGonC/src/include/rogue.h
+++ classicRogueLikeRPGonC/src/include/rogue.h
@@ -12,12 +12,12 @@
 
 // MARK: - Structs
 
-typedef struct {
+typedef struct Position {
     int y;
     int x;
 } Position; // entity to represent hero's position
 
-typedef struct {
+typedef struct Tile {
     char character; // character to represent a specific tiles on the map
     int color; // store a result of ncurses function COLOR_PAIR()
     bool walkable; // player can walk on floors but not  through the walls
@@ -26,7 +26,7 @@ typedef struct {
     bool seen;
 } Tile; // represent toles of the map (should be represented by two dimensional-array)
 
-typedef struct {
+typedef struct Room {
     int height;
     int width;
     Position position; // upper-left corner of the room
@@ -35,7 +35,7 @@ typedef struct {
     // Item ** items; // i should understand why we need **array here
 } Room;
 
-typedef struct {
+typedef struct  Entity {
     Position position; // current hero's position
     char character; // representation of the hero on screen
     int color; // store a result of ncurses function COLOR_PAIR()
