e147887afdf9edab0bff9717d8d4b4f67bc29dce
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-12 08:55:42 +0300

    Change position of enemy

--- classicRogueLikeRPGonC/src/enemy.c
+++ classicRogueLikeRPGonC/src/enemy.c
@@ -4,8 +4,8 @@ Enemy *createEnemy(Position startPosition) {
 
     Enemy *newEnemy = calloc(1, sizeof(Enemy));
 
-    newEnemy->position.y = startPosition.y;
-    newEnemy->position.x = startPosition.x;
+    newEnemy->position.y = startPosition.y += 1;
+    newEnemy->position.x = startPosition.x += 1;
     newEnemy->character = 'O';
     newEnemy->health = 10;
     newEnemy->attack = 1;
